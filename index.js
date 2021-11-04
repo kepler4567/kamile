2c12b47aac4fa1ccd16b2577d83f38d14f502249
kepler4567 <kepler.batista456@gmail.com>
2021-11-04 11:47:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('/fotos', express.static('k&k'))
+
+app.get('/', (req, res) => {
+  
+    res.sendFile(__dirname + "/kamile.html")
+})
+
+app.listen(port, () => {
+  console.log(`Servidor Rodando`)
+})
