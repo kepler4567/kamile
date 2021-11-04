5e295def2a18321f7ec468a03dee6c05fd7dff4c
kepler4567 <kepler.batista456@gmail.com>
2021-11-04 12:01:31 -0300

    Adicionado Fotos

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/kamile.html")
 })
 
+app.get('/fotos', (req, res) => {
+  
+    res.sendFile(__dirname + "/Fotos.html")
+})
+
 app.listen(port, () => {
   console.log(`Servidor Rodando`)
 })
